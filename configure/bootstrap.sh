1fa2137f466755a8a4d602a876495eb7fed228c8
Aarnav Tale <aarnav@tale.me>
2022-01-04 22:17:35 -0500

    feat: initial linux support

--- configure/bootstrap.sh
+++ configure/bootstrap.sh
@@ -3,7 +3,11 @@ cd ~
 shopt -s dotglob
 
 notify() {
-	echo -e "\n\033[0;34m[i]\033[0m $1" >&2
+	echo -e "\033[0;34m[i]\033[0m $1" >&2
+}
+
+error() {
+	echo -e "\033[0;31m[x]\033[0m $1" >&2
 }
 
 DOTDIR="$HOME/.dotfiles"
@@ -37,7 +41,7 @@ command rm -rf "$HOME/.gitconfig"
 command ln -s "$DOTDIR/.gitconfig" "$HOME/.gitconfig"
 command git config --global core.excludesfile $DOTDIR/.gitignore
 command git config --global core.hooksPath $DOTDIR/githooks
-command chmod +x "$DOTDIR/githooks/*"
+command chmod +x $DOTDIR/githooks/* # Can't be in quotes to fix bashisms in Linux
 
 command rm -rf "$HOME/.huskyrc"
 command ln -s "$DOTDIR/.huskyrc" "$HOME/.huskyrc"
@@ -45,4 +49,19 @@ echo "source ~/.dotfiles/.zshrc" > "$HOME/.zshrc"
 
 if [[ $(uname -s) == "Darwin" ]]; then
 	source "$DOTDIR/configure/macos.sh"
+fi
+
+if [[ $(uname -s) == "Linux" ]]; then
+	source /etc/os-release
+	if [[ $ID != "ubuntu" && $ID_LIKE != "debian" ]]; then
+		error "Not running on Ubuntu (Debian Like)"
+		exit 2
+	fi
+
+	if [[ $VERSION_ID != "22.04" && $VERSION_ID != "20.04" ]]; then
+		error "Not running on Focal Fossa (20.04) or Jammy Jellyfish (22.04)"
+		exit 3
+	fi
+
+	source "$DOTDIR/configure/linux.sh"
 fi
--- zshrc.d/linux.zsh
+++ zshrc.d/linux.zsh
@@ -1,2 +1,5 @@
-# Workaround a weird GPG issue
+# Workaround a weird GPG issue when
 export GPG_TTY=$(tty)
+
+alias cat='batcat' # bat(1) is named as batcat on Ubuntu
+alias fd='fdfind' # fd(1) is named as fdfind on Ubuntu
