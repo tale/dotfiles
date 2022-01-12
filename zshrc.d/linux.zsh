680b5a0d8f309447e94680ad7f5a6a2cd29a45d8
Aarnav Tale <aarnav@tale.me>
2022-01-12 14:14:00 -0500

    feat: fix aliases on linux

--- zshrc.d/linux.zsh
+++ zshrc.d/linux.zsh
@@ -1,5 +1,10 @@
-# Workaround a weird GPG issue when
-export GPG_TTY=$(tty)
+# Workaround non TTY GPG
+if [[ -z "$GPG_TTY" ]]; then
+	export GPG_TTY=$(tty)
+fi
 
 alias cat='batcat' # bat(1) is named as batcat on Ubuntu
+alias bat='batcat'
+
 alias fd='fdfind' # fd(1) is named as fdfind on Ubuntu
+alias find='fdfind'
