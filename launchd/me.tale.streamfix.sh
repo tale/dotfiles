bbacda25d24d5c547e60677599f9e2ecd132abd6
Aarnav Tale <aarnav@tale.me>
2022-03-12 11:32:16 -0500

    fix: simplify streamfix logic and make it work

--- launchd/me.tale.streamfix.sh
+++ launchd/me.tale.streamfix.sh
@@ -1,33 +1,17 @@
 #!/bin/bash
-if test $(pgrep -x -- parsecd | wc -l) -eq 2; then
-	# When Parsec runs 2 instances of the Daemon, we are in remote play
-	# Many people suggested disabling Location Services & AirDrop to stop stuttering
-	# This network interface controls that ability so we disable it
+# We check for 2 instances of parsecd because the host daemon is always running
+# If the actual application is open, there will be 2 instances of parsecd
+if test $(pgrep -x -- parsecd | wc -l) -eq 2 || test $(pgrep Moonlight); then
 	if ifconfig awdl0 | grep "status: active" > /dev/null; then
 		ifconfig awdl0 down
-		printf '[%s] (%s) %s\n' "me.tale.streamfix" "$(date '+%H:%M:%S')" "'awdl0' disabled after Parsec startup"
-		exit 0
-	fi
-
-else
-	if ifconfig awdl0 | grep "status: inactive" > /dev/null; then
-		ifconfig awdl0 up
-		printf '[%s] (%s) %s\n' "me.tale.streamfix" "$(date '+%H:%M:%S')" "'awdl0' enabled after Parsec shutdown"
-		exit 0
-	fi
-fi
-
-if test $(pgrep Moonlight); then
-	# Moonlight is a normal app and only runs one instance.
-	if ifconfig awdl0 | grep "status: active" > /dev/null; then
-		ifconfig awdl0 down
-		printf '[%s] (%s) %s\n' "me.tale.streamfix" "$(date '+%H:%M:%S')" "'awdl0' disabled after Moonlight startup"
+		printf '[%s] (%s) %s\n' "me.tale.streamfix" "$(date '+%H:%M:%S')" "'awdl0' disabled"
 		exit 0
 	fi
 else
 	if ifconfig awdl0 | grep "status: inactive" > /dev/null; then
 		ifconfig awdl0 up
-		printf '[%s] (%s) %s\n' "me.tale.streamfix" "$(date '+%H:%M:%S')" "'awdl0' enabled after Moonlight shutdown"
+		printf '[%s] (%s) %s\n' "me.tale.streamfix" "$(date '+%H:%M:%S')" "'awdl0' enabled"
 		exit 0
 	fi
 fi
+
