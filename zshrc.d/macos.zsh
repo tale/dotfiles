073d4c570dbbc0add15ecf979dff7dd254f3f539
Aarnav Tale <aarnav@tale.me>
2022-07-05 11:51:35 -0400

    fix: use HOMEBREW_PREFIX instead of calculating it myself

--- zshrc.d/macos.zsh
+++ zshrc.d/macos.zsh
@@ -3,26 +3,18 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_INSTALL_FROM_API=1
 export HOMEBREW_NO_ENV_HINTS=1
 
-# Better than brew --prefix because it isn't slow
-if [[ $CPUTYPE == "x86_64" ]];
-then
-	export BREW_PREFIX="/usr/local"
-else
-	export BREW_PREFIX="/opt/homebrew"
-fi
-
 # Brew path for manual linking overrides
-export PATH="$BREW_PREFIX/opt/gnu-tar/libexec/gnubin:$PATH"
-export PATH="$BREW_PREFIX/opt/coreutils/libexec/gnubin:$PATH"
-export PATH="$BREW_PREFIX/opt/findutils/libexec/gnubin:$PATH"
-export PATH="$BREW_PREFIX/opt/gnu-which/libexec/gnubin:$PATH"
-export PATH="$BREW_PREFIX/opt/make/libexec/gnubin:$PATH"
-export PATH="$BREW_PREFIX/opt/gnu-tar/libexec/gnubin:$PATH"
-export PATH="$BREW_PREFIX/opt/ruby/bin:$PATH"
+export PATH="$HOMEBREW_PREFIX/opt/gnu-tar/libexec/gnubin:$PATH"
+export PATH="$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="$HOMEBREW_PREFIX/opt/findutils/libexec/gnubin:$PATH"
+export PATH="$HOMEBREW_PREFIX/opt/gnu-which/libexec/gnubin:$PATH"
+export PATH="$HOMEBREW_PREFIX/opt/make/libexec/gnubin:$PATH"
+export PATH="$HOMEBREW_PREFIX/opt/gnu-tar/libexec/gnubin:$PATH"
+export PATH="$HOMEBREW_PREFIX/opt/ruby/bin:$PATH"
 
 # Golang environment
 export GOPATH=$HOME/go
-export GOROOT="$BREW_PREFIX/opt/go/libexec"
+export GOROOT="$HOMEBREW_PREFIX/opt/go/libexec"
 export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
 
 # Cheating lowercase Developer folder shortcut
