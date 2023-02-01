5e1d9372b090f53c991de1cc5665f77f8aeaed58
Aarnav Tale <aarnav@tale.me>
2023-01-31 22:48:31 -0500

    chore: update configs and migrate to config/ dir

---
+++ config/zsh/.zshenv
@@ -0,0 +1,5 @@
+export ZDOTDIR="$HOME/.config/zsh"
+export DOTDIR="$HOME/.config/dotfiles"
+export OS=$(uname -s)
+export PATH="$HOME/.local/bin:$PATH"
+export LESSHISTFILE=-
--- config/zsh/macos.zsh
+++ config/zsh/macos.zsh
@@ -4,6 +4,9 @@ export "SSH_AUTH_SOCK=${HOME}/.gnupg/S.gpg-agent.ssh"
 gpgconf --launch gpg-agent
 
 export THEOS="$HOME/Library/Theos"
+
+# Source brew shellenv
+eval "$(/opt/homebrew/bin/brew shellenv)"
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_INSTALL_FROM_API=1
 export HOMEBREW_NO_ENV_HINTS=1
@@ -22,14 +25,18 @@ export GOPATH=$HOME/go
 export GOROOT="$HOMEBREW_PREFIX/opt/go/libexec"
 export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
 
-# Bun enviroment
+# JS environment
 [ -s "/Users/tale/.bun/_bun" ] && source "/Users/tale/.bun/_bun"
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
 
+export PNPM_HOME="$HOME/Library/pnpm"
+export PATH="$PNPM_HOME:$PATH"
+
 # Cheating lowercase Developer folder shortcut
 export d="$HOME/Developer"
 
+# Workaround a dumb DNS cache lifetime issue
 function plsdns() {
 	command sudo dscacheutil -flushcache
 	command sudo killall -HUP mDNSResponder
