9e288eb18e476e71b04425594eb9ba9419c9732b
Aarnav Tale <aarnav@tale.me>
2023-02-01 12:52:00 -0500

    chore: remove unused launchd script

--- launchd/me.tale.appearance.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $(defaults read -g AppleInterfaceStyle 2>/dev/null) == "Dark" ]]; then
-	git config --global delta.light false
-else
-	git config --global delta.light false
-fi
-
-printf '[%s] (%s) %s\n' "me.tale.appearance" "$(date '+%H:%M:%S')" "Updated non-dynamic interfaces"
