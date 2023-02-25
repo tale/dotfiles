7b0895b91d39d6f30b885d083e4fb406fe05f076
Aarnav Tale <aarnavtale@icloud.com>
2023-02-25 17:33:38 -0500

    chore: import the raycast code script

---
+++ config/raycast_launch.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title code CLI
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 💻
+# @raycast.argument1 { "type": "text", "placeholder": "path" }
+
+# Documentation:
+# @raycast.description Open a file/directory in Visual Studio Code
+# @raycast.author Aarnav Tale
+# @raycast.authorURL https://aarnavtale.com
+
+# disable all output
+exec 1> /dev/null
+
+source ~/.zshenv &> /dev/null
+source "$ZDOTDIR/.zshrc" &> /dev/null
+command /Applications/Neovide.app/Contents/MacOS/neovide --maximized --notabs --multigrid $(eval echo "$1") &> /dev/null
