7f614d1b3902439963d7bbb03c043ec730c878d5
Aarnav Tale <aarnavtale@icloud.com>
2023-02-26 01:56:01 -0500

    fix: git-delta is not on apt download it manually

--- bootstrap/linux.sh
+++ bootstrap/linux.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-APT_LIST="bat exa fd-find fzf git git-delta gnupg2 htop less make build-essential neofetch neovim tmux unzip watch wget zip zstd"
+APT_LIST="bat exa fd-find fzf git gnupg2 htop less make build-essential neofetch neovim tmux unzip watch wget zip zstd"
 
 if [[ $SUDO == "UNSET" ]]; then
 	error "Cannot configure Linux without sudo permissions"
@@ -8,14 +8,20 @@ fi
 
 # Install ZSH if necessary
 if [[ -z $(command -v zsh) ]]; then
-	warn "Missing 'zsh'"
+	notify "Installing ZSH"
 	APT_LIST="$APT_LIST zsh"
 fi
+
+# This assumes we are on an arm64 machine and arm64 docker container!
+DELTA_DEB="https://github.com/dandavison/delta/releases/download/0.15.1/git-delta_0.15.1_arm64.deb"
 
 notify "Updating distribution packages"
 command $SUDO apt-get update && $SUDO apt-get dist-upgrade -yq
 
 notify "Installing $APT_LIST"
+command curl -sL "$DELTA_DEB" -o /tmp/delta.deb
+command $SUDO dpkg -i /tmp/delta.deb
+
 command $SUDO apt-get install -yq $APT_LIST
 command chsh -s $(which zsh)
 
