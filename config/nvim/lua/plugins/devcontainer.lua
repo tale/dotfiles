52ff0023c63234779fd93ec6eb6bd55c4b90ffd9
Aarnav Tale <aarnavtale@icloud.com>
2023-02-25 21:35:48 -0500

    feat: add support for devcontainer

---
+++ config/nvim/lua/plugins/devcontainer.lua
@@ -0,0 +1,9 @@
+return {
+	"https://codeberg.org/esensar/nvim-dev-container",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter"
+	},
+	config = function()
+		require("devcontainer").setup({})
+	end
+}
