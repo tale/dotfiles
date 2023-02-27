de4a81c911e9c440e911f91209375b57c043e122
Aarnav Tale <aarnavtale@icloud.com>
2023-02-27 12:47:56 -0500

    feat: add window-picker ability like nvim-tree

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -3,7 +3,17 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim"
+		"MunifTanjim/nui.nvim",
+		{
+			"s1n7ax/nvim-window-picker",
+			config = function()
+				require("window-picker").setup({
+					autoselect_one = true,
+					include_current_win = false,
+					other_win_hl_color = "#2e3238"
+				})
+			end
+		}
 	},
 	config = function()
 		-- Disable v1 commands from neo-tree
@@ -16,15 +26,42 @@ return {
 
 		-- Command + Shift + E for file explorer
 		vim.keymap.set("n", "<D-s-e>", ":Neotree<CR>")
+		vim.api.nvim_create_autocmd("BufEnter", {
+			pattern = { "*" },
+			desc = "Open a blank buffer to preserve the window picker layout",
+			callback = function()
+				-- Runs only when the last window is neo-tree
+				if vim.fn.winnr("$") == 1 and vim.bo.filetype == "neo-tree" then
+					local winid = vim.api.nvim_get_current_win()
+
+					vim.schedule_wrap(function()
+						vim.cmd("vnew")
+						vim.api.nvim_win_set_width(winid, 30)
+					end)()
+				end
+			end
+		})
 
 		require("neo-tree").setup({
 			window = {
-				position = "right"
+				position = "right",
+				width = 30,
+				mapping_options = {
+					noremap = true,
+					nowait = true
+				},
+				mappings = {
+					["<CR>"] = "open_with_window_picker",
+					["w"] = "open",
+					["<C-x>"] = "open_split",
+					["<C-v>"] = "open_vsplit"
+				}
 			},
 			filesystem = {
 				use_libuv_file_watcher = true,
 				filtered_items = {
 					hide_dotfiles = false,
+					visible = true,
 					never_show = {
 						".DS_Store"
 					},
