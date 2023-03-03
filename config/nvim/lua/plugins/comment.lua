8c1c23ffa6410462f246ae87839c2fcecd29967e
Aarnav Tale <aarnavtale@icloud.com>
2023-03-02 19:50:50 -0500

    feat: set keybinds for commenting via command + /

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -2,5 +2,8 @@ return {
 	"numToStr/Comment.nvim",
 	config = function()
 		require("Comment").setup()
+
+		vim.keymap.set("n", "<D-/>", require("Comment.api").toggle.linewise.current, { noremap = true, silent = true })
+		vim.keymap.set("v", "<D-/>", require("Comment.api").toggle.linewise.current, { noremap = true, silent = true })
 	end
 }
