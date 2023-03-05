d7cafe6cc2e4b85d77c0c8a1dd9fa56ab7906bd3
Aarnav Tale <aarnavtale@icloud.com>
2023-03-05 16:44:35 -0500

    feat: redo lsp bindings

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -60,26 +60,19 @@ return {
 		vim.api.nvim_create_autocmd("LspAttach", {
 			desc = "LSP actions",
 			callback = function()
-				local bufmap = function(mode, lhs, rhs)
-					local opts = { buffer = true }
-					vim.keymap.set(mode, lhs, rhs, opts)
-				end
-
-				vim.keymap.set("n", "<D-.>", vim.diagnostic.open_float, { noremap = true, silent = true })
-				vim.keymap.set("n", "<S-D-s>", vim.lsp.buf.code_action, { noremap = true, silent = true })
-
-				bufmap("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
-				bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
-				bufmap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>")
-				bufmap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>")
-				bufmap("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>")
-				bufmap("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>")
-				bufmap("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>")
-				bufmap("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>")
-				bufmap("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>")
-				bufmap("x", "<F4>", "<cmd>lua vim.lsp.buf.range_code_action()<cr>")
-				bufmap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
-				bufmap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
+				vim.g.bind_keys({
+					{ { "n" }, "<D-.>",   vim.diagnostic.open_float },
+					{ { "n" }, "<S-D-S>", vim.lsp.buf.code_action },
+					{ { "v" }, "<S-D-S>", vim.lsp.buf.range_code_action },
+					{ { "n" }, "<F2>",    vim.lsp.buf.rename },
+					{ { "n" }, "gd",      vim.lsp.buf.definition },
+					{ { "n" }, "gD",      vim.lsp.buf.declaration },
+					{ { "n" }, "gi",      vim.lsp.buf.implementation },
+					{ { "n" }, "go",      vim.lsp.buf.type_definition },
+					{ { "n" }, "gr",      vim.lsp.buf.references },
+					{ { "n" }, "gs",      vim.lsp.buf.signature_help },
+					{ { "n" }, "K",       vim.lsp.buf.hover },
+				})
 			end
 		})
 	end
