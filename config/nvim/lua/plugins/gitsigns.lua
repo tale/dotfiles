f5686fbafc0df4ec6c7e08d5a9f6625bb332d308
Aarnav Tale <aarnavtale@icloud.com>
2023-03-15 17:47:24 -0400

    feat: use blame feature in gitsigns

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -1,11 +1,17 @@
 return {
 	"lewis6991/gitsigns.nvim",
+	event = "BufRead",
 	config = function()
 		vim.opt.signcolumn = "yes"
 
 		require("gitsigns").setup({
 			signcolumn = true,
-			numhl = true,
+			current_line_blame = true,
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol",
+				delay = 250
+			},
 		})
 	end
 }
