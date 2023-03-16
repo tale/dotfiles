118856b1987d5b8a3f07663a629c3aa8df017147
Aarnav Tale <aarnavtale@icloud.com>
2023-03-16 02:03:02 -0400

    fix: tree-sitter and mason should autoinstall

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -24,7 +24,11 @@ return {
 				border = "rounded"
 			}
 		})
-		require("mason-lspconfig").setup()
+
+		require("mason-lspconfig").setup({
+			automatic_installation = true
+		})
+
 		require("mason-lspconfig").setup_handlers({
 			function(server_name)
 				require("lspconfig")[server_name].setup {}
--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -11,6 +11,7 @@ return {
 		update()
 
 		require("nvim-treesitter.configs").setup({
+			auto_install = true,
 			highlight = {
 				enable = true,
 				additional_vim_regex_highlighting = false
