8fe83373cfad826d0ee6f401bbb2290a0b8026b8
Aarnav Tale <aarnavtale@icloud.com>
2023-05-21 13:45:12 -0400

    feat: backup more directories

--- launchd/me.tale.backup.sh
+++ launchd/me.tale.backup.sh
@@ -6,18 +6,19 @@ export B2_ACCOUNT_ID=$(security find-generic-password -s b2-id -w)
 export B2_ACCOUNT_KEY=$(security find-generic-password -s b2-key -w)
 
 RESTIC_EXCLUDE="$DOTDIR/config/restic/excludes.txt"
-DEVELOPER_PATH="$HOME/Developer"
+RESTIC_PATHS=(
+	"$HOME/Developer"
+	"$HOME/Documents"
+	"$HOME/Pictures"
+	"$HOME/.kube"
+	"$HOME/.gnupg" 
+	"$HOME/.ssh"
+)
 
 ledger () {
 	printf '[%s] (%s) %s\n' "me.tale.backup" \
 		"$(date '+%Y-%m-%d %H:%M:%S')" "$1" >> $LEDGER_PATH
-	exit 1
 }
-
-if [[ ! -d $DEVELOPER_PATH ]]; then
-	ledger "Developer path not found."
-	exit 1
-fi
 
 if [[ ! -f $RESTIC_EXCLUDE ]]; then
 	ledger "Restic exclude file not found."
@@ -34,24 +35,32 @@ if [[ -z $RESTIC_PASSWORD ]]; then
 	exit 1
 fi
 
-command restic -r sftp:ftp.tale.me:/tale/restic backup $DEVELOPER_PATH \
-	--exclude-file $RESTIC_EXCLUDE \
-	--verbose
+for BACKUP_PATH in $RESTIC_PATHS; do
+	if [[ ! -d $BACKUP_PATH ]]; then
+		ledger "Restic path not found: $BACKUP_PATH"
+		exit 1
+	fi
 
-if [[ $? -ne 0 ]]; then
-	ledger "SFTP Backup failed."
-	exit 1
-else
-	ledger "SFTP Backup succeeded."
-fi
+	command restic -r sftp:ftp.tale.me:/tale/restic backup $BACKUP_PATH \
+		--exclude-file $RESTIC_EXCLUDE \
+		--verbose
 
-command restic -r b2:tale-ftp:/tale/restic backup $DEVELOPER_PATH \
-	--exclude-file $RESTIC_EXCLUDE \
-	--verbose
+	if [[ $? -ne 0 ]]; then
+		ledger "SFTP Backup failed."
+		exit 1
+	else
+		ledger "SFTP Backup succeeded."
+	fi
 
-if [[ $? -ne 0 ]]; then
-	ledger "B2 Backup failed."
-	exit 1
-else
-	ledger "B2 Backup succeeded."
-fi
+	command restic -r b2:tale-ftp:/tale/restic backup $BACKUP_PATH \
+		--exclude-file $RESTIC_EXCLUDE \
+		--verbose
+
+	if [[ $? -ne 0 ]]; then
+		ledger "B2 Backup failed."
+		exit 1
+	else
+		ledger "B2 Backup succeeded."
+	fi
+done
+
