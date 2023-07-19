c4b40f7c70294d5f4247a2f69a69f6758aa37aca
Aarnav Tale <aarnavtale@icloud.com>
2023-07-19 12:25:49 -0400

    chore: fix objc comments

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -5,6 +5,9 @@ return {
 		"JoosepAlviste/nvim-ts-context-commentstring"
 	},
 	config = function()
+		local ft = require("Comment.ft")
+		ft.set("objc", { "//%s", "/*%s*/" })
+
 		require("Comment").setup({
 			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 		})
