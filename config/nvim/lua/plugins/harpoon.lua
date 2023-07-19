3370141cfd9585fa5f5ffe0cde1c97689bc6e0d4
Aarnav Tale <aarnavtale@icloud.com>
2023-07-19 18:20:27 -0400

    feat: replace neo-tree with oil

--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -11,7 +11,7 @@ return {
 				"TelescopePrompt",
 				"TelescopeResults",
 				"TelescopePreview",
-				"neo-tree",
+				"oil",
 				"terminal"
 			}
 		})
---
+++ config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,35 @@
+return {
+	"stevearc/oil.nvim",
+	event = "VimEnter",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons"
+	},
+	config = function()
+		local oil = require("oil")
+
+		oil.setup({
+			delete_to_trash = true,
+			trash_command = "trash",
+			columns = {
+				"icon",
+				"size",
+				"mtime"
+			},
+			keymaps = {
+				["<C-x>"] = function() oil.select({ horizontal = true, close = true }) end,
+				["<C-v>"] = function() oil.select({ vertical = true, close = true }) end,
+			}
+		})
+
+		vim.g.bind_keys({
+			{ { "n", "i", "t", "v", "x" }, "<C-e>", function()
+				if vim.bo.filetype == "oil" then
+					oil.close()
+				else
+					oil.open()
+				end
+			end
+			}
+		})
+	end
+}
