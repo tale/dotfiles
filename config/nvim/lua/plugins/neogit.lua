9d21ed2ba5fd22c61d522e3f0a60510bd4db3ce6
Aarnav Tale <aarnavtale@icloud.com>
2023-07-20 16:50:06 -0400

    feat: expand diffview with neogit

--- config/nvim/lua/plugins/neogit.lua
+++ config/nvim/lua/plugins/neogit.lua
@@ -1,23 +1,23 @@
 return {
-	"sindrets/diffview.nvim",
+	"NeogitOrg/neogit",
 	keys = {
 		"<Leader>df"
 	},
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
+		"sindrets/diffview.nvim",
 		"nvim-lua/plenary.nvim"
 	},
 	config = function()
 		require("diffview").setup()
+		require("neogit").setup({
+			integrations = {
+				diffview = true
+			}
+		})
 		vim.g.bind_keys({
 			{ { "n", "v" }, "<Leader>df", function()
-				local view = require("diffview.lib").get_current_view()
-
-				if view then
-					vim.cmd("DiffviewClose")
-				else
-					vim.cmd("DiffviewOpen")
-				end
+				require("neogit").open()
 			end },
 		})
 	end
