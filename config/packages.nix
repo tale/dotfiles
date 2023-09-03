33efac4d69102fe470736bbbe83480a182ea2a3e
Aarnav Tale <aarnavtale@icloud.com>
2023-09-03 15:20:46 -0400

    feat: run iconset on macos darwin-rebuild

--- config/packages.nix
+++ config/packages.nix
@@ -26,6 +26,33 @@ let
       license = lib.licenses.mit;
     };
   };
+
+  iconset = pkgs.stdenv.mkDerivation rec {
+    pname = "iconset";
+    version = "1.0.0";
+
+    src = pkgs.fetchzip {
+      url = "https://github.com/tale/iconset/releases/download/v1.0.0/iconset.zip";
+      sha256 = "sha256-/1Qnc3t99FQEM1/l/XtXF28rV4AnCVGtcsr2Drvvs6M=";
+    };
+
+    dontPatch = true;
+    dontConfigure = true;
+    dontBuild = true;
+    dontFixup = true;
+
+    installPhase = ''
+      mkdir -p $out/bin
+      install -m 0755 iconset $out/bin
+    '';
+
+    meta = {
+      homepage = "https://github.com/tale/iconset";
+      description = "A nifty command-line tool to customize macOS icons";
+      platforms = lib.platforms.darwin;
+      license = lib.licenses.asl20;
+    };
+  };
 in
 {
   fonts.fontconfig.enable = true;
@@ -60,7 +87,18 @@ in
     ldid
     libusbmuxd
     macos-trash
+    iconset
     temurin-bin-17
     (nerdfonts.override { fonts = [ "Mononoki" ]; })
   ];
+
+
+  # TODO: Include the application icons in this repository
+  home.activation =
+    if pkgs.stdenv.isDarwin then {
+      # Sudo can be hardcoded here since this runs on macOS
+      iconsetRun = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        $DRY_RUN_CMD /usr/bin/sudo ${iconset}/bin/iconset folder $HOME/Pictures/appicons
+      '';
+    } else { };
 }
