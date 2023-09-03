c8d504a62982e98b91e77b5b8fc7ec3afdbe2a7d
Aarnav Tale <aarnavtale@icloud.com>
2023-09-03 13:59:38 -0400

    feat: add more darwin configurations

--- config/macbook.nix
+++ config/macbook.nix
@@ -44,15 +44,80 @@
     ];
   };
 
-  users.users.tale = {
-    name = "tale";
-    home = "/Users/tale";
+  networking = {
+    computerName = "Aarnav's MacBook Pro";
+    localHostName = "Aarnavs-MBP";
+    hostName = "Aarnavs-MBP";
+    knownNetworkServices = [
+      "Wi-Fi"
+      "Ethernet Adaptor"
+    ];
+    dns = [
+      "1.1.1.1"
+      "1.0.0.1"
+      "8.8.8.8"
+      "8.8.4.4"
+    ];
   };
 
+  nix = {
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+
+    gc = {
+      automatic = true;
+      interval = {
+        Hour = 3;
+        Minute = 0;
+      };
+    };
+  };
+
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # TODO: programs.ssh & services.openssh
   programs.zsh = {
     enable = true;
     promptInit = "";
     enableCompletion = false; # Handled by local zshrc after zcompile runs
+  };
+
+  # Will reset on macOS updates
+  security.pam.enableSudoTouchIdAuth = true;
+
+  # TODO: Maybe look into services.dnsmasq? Or just do it per project
+  system.defaults = {
+    SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
+    dock = {
+      autohide = true;
+      autohide-delay = 0.0;
+      autohide-time-modifier = 0.5;
+      showhidden = true;
+      wvous-tl-corner = 12; # Notification Center
+      wvous-tr-corner = 12;
+      wvous-bl-corner = 11; # Launchpad
+      wvous-br-corner = 11;
+    };
+
+    finder = {
+      ShowPathbar = true;
+      ShowStatusBar = true;
+    };
+
+    menuExtraClock.ShowSeconds = true;
+    screencapture.type = "png";
+  };
+
+  time.timeZone = "America/New_York";
+
+  # TODO: Add OpenSSH things
+  users.users.tale = {
+    name = "tale";
+    home = "/Users/tale";
   };
 
   home-manager.useUserPackages = true;
