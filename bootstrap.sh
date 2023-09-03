099deec645411d4219ffab647c1ad465e671c28c
port19 <port19@port19.xyz>
2023-09-03 21:01:09 +0200

    remove superfluous  "command" and slightly linearize

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,29 +11,18 @@ warn() {
 
 error() {
 	echo -e "\033[0;31m[x]\033[0m $1" >&2
+	exit 1
 }
 
 # Checks before running the script
-if [[ -z "$HOME" ]]; then
-	error "\$HOME is not set"
-	exit 1
-fi
+[[ -z "$HOME" ]] && error "\$HOME is not set"
 
 if [[ -z "$IGNORE_DOTDIR" && ! -z "$DOTDIR" ]]; then
-	error "\$DOTDIR is already set"
-	error "Dotfiles should already be configured"
-	exit 1
+	error "\$DOTDIR is already set\nDotfiles should already be configured"
 fi
 
-if ! $(command -v git > /dev/null 2>&1); then
-	error "'git' is not installed"
-	exit 1
-fi
-
-if ! $(command -v curl > /dev/null 2>&1); then
-	error "'curl' is not installed"
-	exit 1
-fi
+command -v git &>/dev/null || error "'git' is not installed"
+command -v curl &>/dev/null || error "'curl' is not installed"
 
 cd "$HOME"
 
@@ -64,11 +53,11 @@ notify "Using directory: $DOTDIR"
 
 if [[ -d "$DOTDIR" ]]; then
 	warn "Removing existing '$DOTDIR'"
-	command rm -rf "$DOTDIR"
+	rm -rf "$DOTDIR"
 fi
 
 notify "Cloning dotfiles from '$DOTFILES_REPO'"
-command git clone --recursive "$DOTFILES_REPO" "$DOTDIR"
+git clone --recursive "$DOTFILES_REPO" "$DOTDIR"
 
 if [[ "$OS" == "Darwin" ]]; then
 		source "$DOTDIR/bootstrap/macos.sh"
@@ -76,77 +65,75 @@ else
 		source /etc/os-release
 		if [[ $ID != "ubuntu" && $ID_LIKE != "debian" ]]; then
 			error "Not running on Ubuntu"
-			exit 1
 		fi
 
 		if [[ $VERSION_ID != "22.04" ]]; then
 			error "Not running on Ubuntu Jammy Jellyfish (22.04)"
-			exit 1
 		fi
 
 		source "$DOTDIR/bootstrap/linux.sh"
 fi
 
 notify "Installing pnpm"
-command curl -fsSL https://get.pnpm.io/install.sh | SHELL=`which zsh` sh -
+curl -fsSL https://get.pnpm.io/install.sh | SHELL=`which zsh` sh -
 source "$HOME/.zshrc"
 
-command pnpm env use --global lts
-command pnpm add -g pnpm # Upgrade pnpm after installation
+pnpm env use --global lts
+pnpm add -g pnpm # Upgrade pnpm after installation
 
 notify "Installing rust"
-command curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
 
 source "$HOME/.cargo/env"
-command rustup default stable
+rustup default stable
 
 # Configure dirty dotfiles
 notify "Configuring shared dotfiles"
 
-command mkdir -p "$HOME/.ssh"
+mkdir -p "$HOME/.ssh"
 
 if [[ ! -f "$HOME/.ssh/config" ]]; then
-	command touch "$HOME/.ssh/config"
+	touch "$HOME/.ssh/config"
 fi
 
 grep -qxF "Include ~/.config/dotfiles/config/ssh.config" "$HOME/.ssh/config" || echo "Include ~/.config/dotfiles/config/ssh.config" | $SUDO tee -a "$HOME/.ssh/config"
 
-command touch "$HOME/.hushlogin"
-command ln -sf "$DOTDIR/config/.huskyrc" "$HOME/.huskyrc"
+touch "$HOME/.hushlogin"
+ln -sf "$DOTDIR/config/.huskyrc" "$HOME/.huskyrc"
 
 notify "Configuring zsh"
-command ln -sf "$DOTDIR/config/zsh/.zshenv" "$HOME/.zshenv"
+ln -sf "$DOTDIR/config/zsh/.zshenv" "$HOME/.zshenv"
 source "$HOME/.zshenv" # Loads $ZDOTDIR
 
-command rm -rf "$ZDOTDIR" && mkdir -p "$ZDOTDIR"
-command ln -sf "$DOTDIR/config/zsh/.zshrc" "$ZDOTDIR/.zshrc"
-command ln -sf "$DOTDIR/config/zsh/.zlogin" "$ZDOTDIR/.zlogin"
+rm -rf "$ZDOTDIR" && mkdir -p "$ZDOTDIR"
+ln -sf "$DOTDIR/config/zsh/.zshrc" "$ZDOTDIR/.zshrc"
+ln -sf "$DOTDIR/config/zsh/.zlogin" "$ZDOTDIR/.zlogin"
 
 notify "Configuring git"
-command rm -rf "$HOME/.gitconfig"
-command ln -sf "$DOTDIR/config/git/.gitconfig" "$HOME/.gitconfig"
+rm -rf "$HOME/.gitconfig"
+ln -sf "$DOTDIR/config/git/.gitconfig" "$HOME/.gitconfig"
 
-command rm -rf "$HOME/.config/git"
-command mkdir -p "$HOME/.config/git"
-command ln -sf "$DOTDIR/config/git/.gitignore" "$HOME/.config/git/.gitignore"
+rm -rf "$HOME/.config/git"
+mkdir -p "$HOME/.config/git"
+ln -sf "$DOTDIR/config/git/.gitignore" "$HOME/.config/git/.gitignore"
 
-command ln -sf "$DOTDIR/config/git/hooks" "$HOME/.config/git/hooks"
+ln -sf "$DOTDIR/config/git/hooks" "$HOME/.config/git/hooks"
 for hook in "$DOTDIR/config/git/hooks/"*; do
-	command chmod +x "$hook"
+	chmod +x "$hook"
 done
 
 notify "Configuring Neovim and Tmux"
-command rm -rf "$HOME/.config/nvim"
-command mkdir -p "$HOME/.config/nvim"
-command ln -sf "$DOTDIR/config/nvim/lua" "$HOME/.config/nvim/lua"
-command ln -sf "$DOTDIR/config/nvim/init.lua" "$HOME/.config/nvim/init.lua"
+rm -rf "$HOME/.config/nvim"
+mkdir -p "$HOME/.config/nvim"
+ln -sf "$DOTDIR/config/nvim/lua" "$HOME/.config/nvim/lua"
+ln -sf "$DOTDIR/config/nvim/init.lua" "$HOME/.config/nvim/init.lua"
 
-command nvim --headless "+Lazy! sync" +qa # Installs plugins via Lazy
-command ln -sf "$DOTDIR/config/tui/tmux.conf" "$HOME/.tmux.conf"
+nvim --headless "+Lazy! sync" +qa # Installs plugins via Lazy
+ln -sf "$DOTDIR/config/tui/tmux.conf" "$HOME/.tmux.conf"
 
 notify "Finished configuration (maybe restart?)"
-command rm -rf "$HOME/.zshrc"
-command rm -rf "$HOME/.zlogin"
+rm -rf "$HOME/.zshrc"
+rm -rf "$HOME/.zlogin"
 
 source "$HOME/.zshenv"
 source "$ZDOTDIR/.zshrc"
