3ac83722c50f7906186f556b0a2e659a0a8171d5
Aarnav Tale <aarnavtale@icloud.com>
2023-10-13 14:38:36 -0400

    fix: indent blankline v3 has a new api

--- config/nvim/lua/plugins/indent-blankline.lua
+++ config/nvim/lua/plugins/indent-blankline.lua
@@ -2,10 +2,10 @@ return {
 	"lukas-reineke/indent-blankline.nvim",
 	event = "BufRead",
 	config = function()
-		require("indent_blankline").setup {
-			char = "▏",
-			buftype_exclude = { "terminal", "nofile" },
-			show_current_context = true,
+		require("ibl").setup {
+			indent = {
+				char = "▏",
+			}
 		}
 	end,
 }
