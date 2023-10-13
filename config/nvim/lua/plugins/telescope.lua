74a5e9dd1414afb099b8aa54df7652837417c122
Aarnav Tale <aarnavtale@icloud.com>
2023-10-13 14:39:04 -0400

    feat: update telescope and cleanup config

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,41 +1,36 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	event = "VimEnter",
-	keys = {
-		"<Leader>gt",
-		"<Leader>ff",
-		"<Leader>fi",
-		"<Leader>cc"
-	},
-	tag = "0.1.1",
+	branch = "0.1.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"stevearc/dressing.nvim",
 		{
 			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "make"
-		}
+			build = "make",
+		},
 	},
-	config = function()
-		require("telescope").load_extension("fzf")
-
-		require("telescope").setup({
-			defaults = {
-				mappings = {
-					i = {
-						["<esc>"] = require("telescope.actions").close
-					}
+	event = "VimEnter",
+	keys = {
+		"<Leader>gt",
+		"<Leader>ff",
+		"<Leader>fi",
+	},
+	opts = {
+		defaults = {
+			mappings = {
+				i = {
+					["<esc>"] = "close",
 				},
-				file_ignore_patterns = {
-					".git/",
-					"node_modules/",
-					"%.class"
-				}
+			},
+			file_ignore_patterns = {
+				".git/",
+				"node_modules/",
+				"%.class",
 			},
 			pickers = {
 				find_files = {
 					hidden = true,
-					smart_cwd = true
+					smart_cwd = true,
 				},
 			},
 			extensions = {
@@ -43,20 +38,23 @@ return {
 					fuzzy = true,
 					override_generic_sorter = false,
 					override_file_sorter = true,
-					case_mode = "smart_case"
-				}
-			}
-		})
-
+					case_mode = "smart_case",
+				},
+			},
+		},
+	},
+	config = function(_, options)
+		require("telescope").setup(options)
+		require("telescope").load_extension("fzf")
 		local builtin = require("telescope.builtin")
 
 		vim.g.bind_keys({
 			{ { "n", "t" }, "<Leader>gt", builtin.find_files },
 			{ { "n", "t" }, "<Leader>ff", builtin.find_files },
 			{ { "n", "t" }, "<Leader>fi", builtin.live_grep },
-			{ { "n" },      "gr",         builtin.lsp_references },
-			{ { "n" },      "gi",         builtin.lsp_implementations },
-			{ { "n" },      "gd",         builtin.lsp_definitions }
+			{ { "n" }, "gr", builtin.lsp_references },
+			{ { "n" }, "gi", builtin.lsp_implementations },
+			{ { "n" }, "gd", builtin.lsp_definitions },
 		})
-	end
+	end,
 }
