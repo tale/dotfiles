8469b29ab5b1e7f941630bae617fc0b008fba259
Aarnav Tale <aarnavtale@icloud.com>
2023-10-26 18:28:45 -0400

    feat: simplify tmux config

--- config/tui/default.nix
+++ config/tui/default.nix
@@ -1,18 +1,8 @@
 { pkgs, lib, config, ... }: {
-  home.packages = with pkgs; [ tmate ];
   home.file.".config/wezterm/wezterm.lua".text = builtins.readFile ./wezterm.lua;
-
   programs.tmux = {
     enable = true;
-    prefix = "C-space";
     shell = "${pkgs.zsh}/bin/zsh";
-    terminal = "screen-256color";
-
-    aggressiveResize = true;
-    baseIndex = 1;
-    escapeTime = 0;
-    historyLimit = 50000;
-    keyMode = "vi";
     extraConfig = builtins.readFile ./tmux.conf;
   };
 }
