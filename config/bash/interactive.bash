71f7b4dc6a354dfc5aadd22d7f0c452694b915ea
Aarnav Tale <aarnavtale@icloud.com>
2023-12-15 14:49:45 -0500

    fix: fzf bash history should replace current cmd

--- config/bash/interactive.bash
+++ config/bash/interactive.bash
@@ -47,18 +47,14 @@ bind '"\C-r": "\C-x1\e^\er"'
 bind -x '"\C-x1": __fzf_history';
 
 __fzf_history () {
-	__ehc $(history | fzf --tac --tiebreak=index | perl -ne 'm/^\s*([0-9]+)/ and print "!$1"')
-}
-
-__ehc() {
-	if [[ -n $1 ]]; then
-        bind '"\er": redraw-current-line'
-        bind '"\e^": magic-space'
-        READLINE_LINE=${READLINE_LINE:+${READLINE_LINE:0:READLINE_POINT}}${1}${READLINE_LINE:+${READLINE_LINE:READLINE_POINT}}
-        READLINE_POINT=$(( READLINE_POINT + ${#1} ))
+	entry=$(history | fzf --tac --tiebreak=index | perl -ne 'm/^\s*([0-9]+)/ and print "!$1"')
+	if [[ -n $entry ]]; then
+		bind '"\er": redraw-current-line'
+		bind '"\e^": magic-space'
+		READLINE_LINE="$entry"
+		READLINE_POINT=${#entry}
 	else
-        bind '"\er":'
-        bind '"\e^":'
+		bind '"\er":'
+		bind '"\e^":'
 	fi
 }
-
