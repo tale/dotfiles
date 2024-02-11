6785dd75f285dd0561ffab5937013636cf6f994e
Aarnav Tale <aarnavtale@icloud.com>
2024-02-10 22:54:57 -0500

    fix: this script was so overcomplicated

--- config/bash/sessionize.sh
+++ config/bash/sessionize.sh
@@ -1,87 +1,31 @@
 #!/usr/bin/env bash
+# Inspired by ThePrimeagen's tmux-sessionizer
 
-if [[ -z "$d" || ! -d "$d" ]]; then
-    echo "Developer directory (\$d) is not set correctly"
-    exit 1
-fi
+sessions="$(tmux ls | awk -F: '{print $1}')"
+dirs=$(find $d -maxdepth 2 -type d | sed "s|^$d|~|")
+sel=$(printf "$sessions\n%s\n" "${dirs[@]}" | fzf)
 
-if ! command -v fzf &>/dev/null; then
-	echo "Could not find 'fzf'"
-	exit 1
-fi
-
-if ! command -v tmux &>/dev/null; then
-	echo "Could not find 'tmux'"
-	exit 1
-fi
-
-dirs=("dotfiles")
-while IFS= read -r dir; do
-	dirs+=("${dir//$d/\~}")
-done < <(find "$d" -maxdepth 2 -type d);
-
-# Open the fzf menu in the developer directory
-dir=$(printf "%s\n" "${dirs[@]}" | fzf --ansi \
-	--border none \
-	--no-scrollbar \
-	--preview-window border-left:40% \
-	--preview "tmux list-sessions -F \"#{session_name} #{window_name} #{window_panes} #{window_active} #{window_flags}\" | \
-		awk '
-		BEGIN {
-			FS=\" \"
-			ORS=\"\"
-		}
-		{
-			if (\$4 == 1) {
-				print \"\033[1;32m\"
-			} else {
-				print \"\033[0;37m\"
-			}
-			print \$1
-			print \"\033[0m\"
-			print \" - \"
-			print \$2
-			print \" (\"
-			print \$3
-			print \")\n\"
-		}'")
-
-
-if [[ -z $dir ]]; then
+if [[ -z $sel ]]; then
 	exit 0
 fi
 
-# Custom session names
-if [[ $dir == "dotfiles" ]]; then
-	session_name="main"
-	dir="$dd"
+dir="${sel//\~/$d}"
+if [[ -d $dir ]]; then
+	prefix=$(basename "$(dirname "$dir")")
+	name=$(basename "$dir" | tr . -)
+
+	session_name="$prefix-$name"
 else
-	# Takes the directory and creates a session name from the last 2 parts
-	dir="${dir//\~/$d}"
-	session_dir_prefix=$(basename "$(dirname "$dir")")
-	session_name="$session_dir_prefix-$(basename "$dir")"
+	session_name="$sel"
 fi
 
-tmux_running="$(pgrep tmux)"
-
-# Tmux is not running, start the session here
-if [[ -z $tmux_running ]]; then
+if [[ -z $(pgrep tmux) ]] || ! tmux has-session -t "$session_name" 2>/dev/null; then
 	tmux new-session -s "$session_name" -c "$dir"
-	tmux attach -t "$session_name"
-	exit 0
+else
+	if [[ -z $TMUX ]]; then
+		tmux attach -t "$session_name"
+	else
+		tmux switch-client -t "$session_name"
+	fi
 fi
-
-# Create the session if it doesn't already exist
-if ! tmux has-session -t "$session_name" 2>/dev/null; then
-	tmux new-session -ds "$session_name" -c "$dir"
-fi
-
-# Attach to session if tmux client isn't running
-if [[ -z $TMUX ]]; then
-	tmux attach -t "$session_name"
-	exit 0
-fi
-
-# Attach to session if tmux client is running
-tmux switch-client -t "$session_name"
 
